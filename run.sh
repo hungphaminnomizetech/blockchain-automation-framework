0aba90227310493b17ee23eb4f98303969f0042a
tien.phu.nguyen01@gmail.com <tien.phu.nguyen01@gmail.com>
2021-06-23 18:07:55 +0700

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: tien.phu.nguyen01@gmail.com <tien.phu.nguyen01@gmail.com>

--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/blockchain-automation-framework/build/config
+KUBECONFIG=/home/tiennguyen/projects/hyperledger/blockchain-automation-framework/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/tiennguyen/projects/hyperledger/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/tiennguyen/projects/hyperledger/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/tiennguyen/projects/hyperledger/blockchain-automation-framework/build/network.yml" -e 'ansible_python_interpreter=/usr/bin/python3'
+
